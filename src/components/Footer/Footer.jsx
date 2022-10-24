7fa9a4cd5b190d16187e66d8f46215815011c620
Okunlola Abdulafeez Shola <sholaafeez81@outlook.com>
2022-10-24 05:53:48 +0100

    my virtual card

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
     <div className='Social-icon'>
         <a className='Social' href="https://www.fb.com/shola.afeez" ><img src={Facebook} alt="" height="50" weight="50" /></a>
         <a className='Social' href="https://www.instagram.com/abdulafeez_ok" ><img src={Instagram} alt="" height="50" weight="50" /></a>
-        <a className='Social' href="https://www.twitter.com/@oash_affable" ><img src={Twitter} alt="" height="50" weight="50" /></a>
+        <a className='Social' href="https://twitter.com/oash_affable" ><img src={Twitter} alt="" height="50" weight="50" /></a>
         <a className='Social' href="https://www.linkedin.com/in/abdulafeez-shola-okunlola-54b13a145" ><img src={Linkedin} alt="" height="50" weight="50" /></a>
         <a className='Social' href="https://wa.me/2348154502738"><img src={Whatsapp} alt="" height="50" weight="50" /></a>
     </div>
